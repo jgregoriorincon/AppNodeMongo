2f76a9826827269d615c53a13fcb8b5d0d0d93eb
jgregoriorincon <jgregoriorincon@gmail.com>
2017-03-16 21:04:57 -0500

    cambio modelo artista

--- models/artist.ts
+++ models/artist.ts
@@ -2,13 +2,13 @@
 
 import {Document, model, Model, Schema} from 'mongoose';
 
-export const ArtistSchema: Schema = new Schema({
+const ArtistSchema: Schema = new Schema({
     name: String,
     description: String,
     image: String
 });
 
-export interface IArtist extends Document {
+interface IArtist extends Document {
     name: String;
     description: String;
     image: String;
